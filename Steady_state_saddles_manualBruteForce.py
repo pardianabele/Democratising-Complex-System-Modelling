3a2483b432c240e449a557cdd346b64bf488db2b
Anabele-Linda Pardi <anabelepardi@gmail.com>
2023-06-04 21:16:40 +0200

    Brute force function plotting

--- Steady_state_saddles_manualBruteForce.py
+++ Steady_state_saddles_manualBruteForce.py
@@ -1,14 +1,33 @@
+from scipy.optimize import fsolve
 import matplotlib.pyplot as plt
 import numpy as np
 
+
 p = 2
 q = 1
-f = 3
+
+f = 1
 g = 2
 d = 1
-t = 2
-a = 100
 
+t = 100 #t = 1000000
+a = 3
+
+def initial_values(t, p, q):
+    k = 1
+    x = k*np.power(t, 1/p)
+    y = k*np.power(t, 1/q)
+    return x, y
+
+def equations(s):
+    x, y, b = s
+    return (np.power(t, 1 / p) - x * np.power(a, f),
+            np.power(t, 1 / q) - y * b * np.power(a, g) - y * np.power(a, d),
+            x * np.power(a, f) - y * b * np.power(a, g))
+
+initial_guess = (0, 0, 0)
+solution = fsolve(equations, initial_guess)
+print(solution)
 
 def saddle_point(x, y, z):
     return (np.power(t, 1 / p) - x * np.power(a, f)) * (
@@ -16,11 +35,19 @@ def saddle_point(x, y, z):
             x * np.power(a, f) - y * z * np.power(a, g))
 
 
-spacing = 50
+spacing = 40
 
-x = np.linspace(-200, 200, spacing)
-y = np.linspace(-200, 200, spacing)
-z = np.linspace(-200, 200, spacing)
+x_limit, y_limit = initial_values(t, p, q)
+print(x_limit, y_limit)
+
+#x = np.linspace(-500, 500, spacing)
+#y = np.linspace(-500, 500, spacing)
+#z = np.linspace(-500, 500, spacing)
+
+x = np.linspace(0, x_limit, spacing)
+y = np.linspace(0, y_limit, spacing)
+z = np.linspace(0, x_limit, spacing)
+
 
 xs = []
 ys = []
@@ -32,9 +59,11 @@ maxVal = 0
 for i in range(len(x)):
     for j in range(len(y)):
         for k in range(len(z)):
-            val = saddle_point(x[i], y[j], z[k]) / 1e25
-            if x[i] < 0 or y[j] < 0 or z[k] < 0:
-                continue
+            val = saddle_point(x[i], y[j], z[k]) / 1e24
+            #if x[i] < 0 or y[j] < 0 or z[k] < 0:
+            #    continue
+            #if val < -0.0001: #> 0.06 or val < 0.05:
+            #    continue
             if val < minVal:
                 minVal = val
             if val > maxVal:
@@ -44,14 +73,34 @@ for i in range(len(x)):
             zs.append(z[k])
             vals.append(val)
 
+print("minVal", minVal)
+print("maxVal", maxVal)
+#print("values", vals)
+
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 
-sc = ax.scatter(xs, ys, zs, c=vals, cmap="viridis", vmin=minVal, vmax=maxVal, s=5)
-plt.colorbar(sc)
+sc = ax.scatter(xs, ys, zs, c=vals, cmap="plasma", vmin=minVal, vmax=maxVal, s=5)
+ax.scatter(solution[0], solution[1], solution[2], c='r', marker='o', s=25, zorder=20)
+# Create the color bar with a padding of 0.05 (adjust as needed)
+cbar = plt.colorbar(sc, pad=0.09)
+
+# Add a label to the color bar
+cbar.set_label('1e24')
+
+# Adjust the position of the color bar using the set_ticks_position() method
+#cbar.ax.yaxis.set_ticks_position('left')
+ax.set_xlim(0, x_limit)  # Set x-axis limits from 0 to 6
+ax.set_ylim(0, y_limit)  # Set y-axis limits from 0 to 12
+ax.set_zlim(0, x_limit)
 
 ax.set_xlabel('x')
 ax.set_ylabel('y')
-ax.set_zlabel('z')
+ax.set_zlabel('b')
+
+title = "Saddle Point Function"
+ax.set_title(f"{title}\n for (\u03b1, \u03b2, f, g, d, a): {p, q, f, g, d, a}")
 
 plt.show()
+
+#---------------------- analysis --------------
---
+++ Strage_attractor.py
@@ -0,0 +1,62 @@
+import numpy as np
+from scipy.integrate import solve_ivp
+import matplotlib.pyplot as plt
+from scipy.optimize import fsolve
+
+p = 2
+q = 1
+
+f = 3
+g = 2
+d = 1
+
+t = 100
+a = 35 
+
+def initial_values(t, p, q):
+    k = 1
+    x = k*np.power(t, 1/p)
+    y = k*np.power(t, 1/q)
+    return x, y
+
+def equations(s):
+    x, y, b = s
+    return (np.power(t, 1 / p) - x * np.power(a, f),
+            np.power(t, 1 / q) - y * b * np.power(a, g) - y * np.power(a, d),
+            x * np.power(a, f) - y * b * np.power(a, g))
+
+initial_guess = (0, 0, 0)
+solution = fsolve(equations, initial_guess)
+print(solution)
+
+# Your system of ODEs
+def F(t, y):
+    x, y, b = y
+
+    dxdt = np.power(t, 1 / p) - x * np.power(a, f)
+    dydt = np.power(t, 1 / q) - y * b * np.power(a, g) - y * np.power(a, d)
+    dbdt = x * np.power(a, f) - y * b * np.power(a, g)
+
+    return [dxdt, dydt, dbdt]
+
+# Initial conditions
+x_limit, y_limit = initial_values(t, p, q)
+y0 = [x_limit, y_limit, 0]
+t = [0, 1000]  # Time range
+
+# Solve the system of equations
+sol = solve_ivp(F, t, y0, dense_output=True)
+
+# Get the solutions
+t = np.linspace(0, 1000, 1000)  # You can adjust the number of points as needed
+y = sol.sol(t)
+
+# Plot the phase space trajectory in 3D
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.scatter(solution[0], solution[1], solution[2], c='r', marker='o', s=25, zorder=20)
+ax.plot(y[0], y[1], y[2])
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+ax.set_zlabel('b')
+plt.show()
