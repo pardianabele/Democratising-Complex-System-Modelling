9290428a0651e5e37acf82a03ee1e45599972947
Anabele-Linda Pardi <anabelepardi@gmail.com>
2024-03-31 20:14:36 +0200

    Update Brusselator
    New plot with several loops

--- Brusselator.py
+++ Brusselator.py
@@ -2,34 +2,28 @@ import numpy as np
 from scipy.integrate import solve_ivp
 import matplotlib.pyplot as plt
 
-
 def equations(t, y, a, b):
-    # y[0] = x, y[1] = y
     dxdt = a - (b + 1) * y[0] + np.power(y[0], 2) * y[1]
     dydt = b * y[0] - np.power(y[0], 2) * y[1]
-
     return [dxdt, dydt]
 
+a = 1.5
+b = 3
+init = [1, 1]
 
-a = 100
-b = 100
-init = [0, 0]  # x0, y0
 
-# Time range to solve for
-t_eval = np.linspace(0, 1, 1000)
+t_span = (0, 10)
+t_eval = np.linspace(0, 10, 500)
 
-# Solve the ODEs
-solution = solve_ivp(equations, (0, 300), init, args=(a, b), t_eval=t_eval, method='Radau')
+solution = solve_ivp(equations, t_span, init, args=(a, b), t_eval=t_eval, method='Radau')
 
-print(solution.y[0])
-print("--------------------------")
-print(solution.y[1])
-
-# Plot the solutions
-plt.plot(solution.t, solution.y[0], label='x', linewidth=3)
-plt.plot(solution.t, solution.y[1], label='y', linewidth=3)
+plt.figure(figsize=(10, 5))
+plt.plot(solution.t, solution.y[0], label='x', linewidth=2)
+plt.plot(solution.t, solution.y[1], label='y', linewidth=2)
 
 plt.xlabel('Time steps')
 plt.ylabel('Concentration (unitless)')
+
 plt.legend()
+plt.grid(True)
 plt.show()
